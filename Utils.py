69fde9ae373bb48aeb313be2ad16a2c9c3016642
liuqing <liuqing@kingsoft.com>
2020-07-03 14:53:25 +0800

    Add graphic support

---
+++ Utils.py
@@ -0,0 +1,22 @@
+import logging
+import coloredlogs
+import sys
+
+global logger
+
+logger = logging.getLogger("Test")
+
+logger.setLevel(level=logging.DEBUG)
+formatter = logging.Formatter("%(asctime)s[%(thread)d] %(levelname)s %(pathname)s[:%(lineno)d] %(message)s")
+
+coloredlogs.DEFAULT_FIELD_STYLES = {'asctime': {'color': 'green'}, 'levelname': {'color': 'yellow', 'bold': True}, 'name': {'color': 'blue'}, 'pathname': {'color': 'cyan'}}
+coloredlogs.install(fmt="%(asctime)s[%(thread)d] %(levelname)s %(pathname)s[:%(lineno)d] %(message)s")
+
+# 输出到控制台
+# streamHandler = logging.StreamHandler(sys.stdout)
+# logger.addHandler(streamHandler)
+
+# 输出到文件
+fileHandler = logging.FileHandler(filename="Log.txt", mode="w", encoding="utf-8")
+fileHandler.setFormatter(formatter)
+logger.addHandler(fileHandler)
